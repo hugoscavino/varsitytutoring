1287341fd23d018245d035faad4807d4cc6cd150
hugoscavino <hugo@scavino.org>
2023-10-09 17:58:07 -0400

    Removed bug

--- src/com/scavino/varisity/student200/RemoveStuff.java
+++ src/com/scavino/varisity/student200/RemoveStuff.java
@@ -3,45 +3,41 @@ package com.scavino.varisity.student200;
 public class RemoveStuff {
 
     public static void main (String[] args){
-        System.out.println("RemoveStuff : " + removeStuff("Hey, how are you?"));
+        //System.out.println("RemoveStuff : " + removeStuff("Hey, how are you?"));
         System.out.println("RemoveStuff : " + removeStuff("Do?n?'?t catch ?fish"));
         System.out.println("RemoveStuff : " + removeStuff("Green? apples"));
     }
 
     private static String removeStuff(String s) {
-        // step 1 - remove the spaces
-        final char characterToRemove = ' ';
 
-        // Step 1.1 - Convert to char array
+        String charsWithoutPrecedingQMark = "";
+        // Step 2 - Remove all characters that are immediately preceded by a question mark.
 
         char[] charactersWithSpacesAndExclaimation = s.toCharArray();
-        String charactersWithoutSpaces = "";
-        for (int i = 0; i < charactersWithSpacesAndExclaimation.length; i++){
-
-            if (s.charAt(i) == characterToRemove){
-                // ignore
-            } else {
-                charactersWithoutSpaces = charactersWithoutSpaces + s.charAt(i);
-            }
-        }
-
-        // Step 2 - We have removed all the spaces now spaces remove (b)
-        // all characters that are immediately preceded by a question mark.
-
-        String result = "";
-
-        char[] charactersWithExclaimation = charactersWithoutSpaces.toCharArray();
         final char characterToPrecede = '?';
-        for (int i = 0; i < charactersWithExclaimation.length; i++){
-            if ( charactersWithoutSpaces.charAt(i) == characterToPrecede){
+        for (int i = 0; i < charactersWithSpacesAndExclaimation.length; i++){
+            if ( charactersWithSpacesAndExclaimation[i] == characterToPrecede){
                 // skip me and skip next one, move the i IF not at the end
-                result = result + charactersWithoutSpaces.charAt(i);
+                charsWithoutPrecedingQMark = charsWithoutPrecedingQMark + charactersWithSpacesAndExclaimation[i];
                 i = i + 1;
-                if (i >= charactersWithExclaimation.length){
+                if (i >= charactersWithSpacesAndExclaimation.length){
                     break;
                 }
             } else {
-                result = result + charactersWithoutSpaces.charAt(i);
+                charsWithoutPrecedingQMark = charsWithoutPrecedingQMark + charactersWithSpacesAndExclaimation[i];
+            }
+        }
+
+        // step 2 - remove the spaces
+        final char characterToRemove = ' ';
+        char[] charactersWithoutSpaces = charsWithoutPrecedingQMark.toCharArray();
+        String result = "";
+        for (int i = 0; i < charactersWithoutSpaces.length; i++){
+
+            if (charactersWithoutSpaces[i] == characterToRemove){
+                // ignore
+            } else {
+                result = result + charactersWithoutSpaces[i];
             }
         }
 
